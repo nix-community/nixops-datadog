c9462e0658dd7ce4679757a6acf9f14013128466
Graham Christensen <graham@grahamc.com>
2020-04-03 17:51:57 -0400

    poetryify

--- nixops_datadog/plugin.py
+++ nixops_datadog/plugin.py
@@ -3,14 +3,8 @@ import nixops.plugins
 
 @nixops.plugins.hookimpl
 def nixexprs():
-    expr_path = os.path.realpath(os.path.dirname(__file__) + "/../../../../share/nix/nixops-datadog")
-    if not os.path.exists(expr_path):
-        expr_path = os.path.realpath(os.path.dirname(__file__) + "/../../../../../share/nix/nixops-datadog")
-    if not os.path.exists(expr_path):
-        expr_path = os.path.dirname(__file__) + "/../nix"
-
     return [
-        expr_path
+            os.path.dirname(__file__) + "/nix"
     ]
 
 @nixops.plugins.hookimpl
--- nixops_datadog/resources/__init__.py
+++ nixops_datadog/resources/__init__.py
@@ -1,3 +1,3 @@
-import datadog_monitor
-import datadog_screenboard
-import datadog_timeboard
+from . import datadog_monitor
+from . import datadog_screenboard
+from . import datadog_timeboard
